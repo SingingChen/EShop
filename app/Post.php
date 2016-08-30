04d97394d870c186a91b98df0528260df9b95602
SingingChen <docterwater@gmail.com>
2016-08-30 16:29:31 +0800

    migrate posts table
    建立Post model

---
+++ app/Post.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: user
+ * Date: 2016/8/30
+ * Time: 下午 02:26
+ */
+
+namespace App;
+
+
+use Illuminate\Auth\EloquentUserProvider;
+
+class Post extends Eloquent
+{
+protected $table ="posts";
+    
+
+}
