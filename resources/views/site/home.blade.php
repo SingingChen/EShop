465cadcdb228ec59c1173f7aad0be76a003628f9
SingingChen <docterwater@gmail.com>
2016-09-01 14:33:01 +0800

    改寫home.blade @extends('site.layouts.default') @section('content') ... @stop 來包住。這裡的 'content' 名稱要和 default.blade.php 中的 @yield('content') 相同，表示要填補這個洞。

--- resources/views/site/home.blade.php
+++ resources/views/site/home.blade.php
@@ -1,22 +1,17 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>{{$title}}</title>
-</head>
-<body>
-<h1>{{$title}}</h1>
-<div>{{Html::link('post/create','新增')}}</div>
-@if(isset($posts))
-<ol>
-    @foreach($posts as $post)
-        <li>{{Html::link('post/'.$post->id,$post->title)}}
-        ({{Html::link('post/'.$post->id.'/edit' , '編輯')}})
-        </li>
-@endforeach
+@extends('site.layouts.default')
 
-</ol>
+@section('content')
+    <h1>{{$title}}</h1>
+    <div>{{Html::link('post/create','新增')}}</div>
+    @if(isset($posts))
+        <ol>
+            @foreach($posts as $post)
+                <li>{{Html::link('post/'.$post->id,$post->title)}}
+                    ({{Html::link('post/'.$post->id.'/edit' , '編輯')}})
+                </li>
+            @endforeach
+
+        </ol>
 
     @endif
-</body>
-</html>
+@stop
