56cbfa1332615dc5594a50c64ec9393ab6e174c3
SingingChen <docterwater@gmail.com>
2016-09-01 09:59:38 +0800

    透過 Route::resource 的方式來換掉前面寫了七行的 route：

--- routes/web.php
+++ routes/web.php
@@ -13,17 +13,19 @@
 
 Route::get('/', 'HomeController@index');
 
-//用group群組化管理
-Route::group(['prefix'=>'post'],function (){
-    Route::get('/', 'HomeController@index');
-    Route::get('create', 'HomeController@create');
-    Route::post('/', 'HomeController@store');
-    Route::get('{id}', 'HomeController@show');
-    Route::get('{id}/edit', 'HomeController@edit');
-    Route::put('{id}', 'HomeController@update');//put ::  你會看到這個 route 是 put 並且網址為 post/{id}，這就是為什麼表單 url 及 method 要修改的理由。
-    Route::delete('{id}' , 'HomeController@destroy');
+Route::resource('post' ,'HomeController');
 
-});
+//用group群組化管理
+//Route::group(['prefix'=>'post'],function (){
+//    Route::get('/', 'HomeController@index');
+//    Route::get('create', 'HomeController@create');
+//    Route::post('/', 'HomeController@store');
+//    Route::get('{id}', 'HomeController@show');
+//    Route::get('{id}/edit', 'HomeController@edit');
+//    Route::put('{id}', 'HomeController@update');//put ::  你會看到這個 route 是 put 並且網址為 post/{id}，這就是為什麼表單 url 及 method 要修改的理由。
+//    Route::delete('{id}' , 'HomeController@destroy');
+
+//});
 //Route::get('post', 'HomeController@index');
 //Route::get('post/create', 'HomeController@create');
 //Route::post('post', 'HomeController@store');
