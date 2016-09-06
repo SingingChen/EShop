163306377a5074bc44a0e38fbe20404bfd98454a
SingingChen <docterwater@gmail.com>
2016-09-06 14:57:25 +0800

    整合 checkout  ,cart頁面

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -5,7 +5,7 @@
 
 <section id="advertisement">
     <div class="container">
-            <img src="{{asset('images/shop/advertisement.jpg')}}" alt="" />
+        <img src="{{asset('images/shop/advertisement.jpg')}}" alt="" />
 
     </div>
 </section>
