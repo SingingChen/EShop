3e9c2e373904a657c03c521a433503658d0160e9
SingingChen <docterwater@gmail.com>
2016-09-06 15:31:13 +0800

    文章列表 連結到 單篇文章

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -27,10 +27,10 @@
 								</span>
 							</div>
 							<a href="">
-								<img src="images/blog/blog-one.jpg" alt="">
+								<img src="{{asset('images/blog/blog-one.jpg')}}" alt="">
 							</a>
 							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
-							<a  class="btn btn-primary" href="">Read More</a>
+							<a  class="btn btn-primary" href="{{url("/blog/post/3")}}">Read More</a>
 						</div>
 						<div class="single-blog-post">
 							<h3>Girls Pink T Shirt arrived in store</h3>
@@ -49,10 +49,12 @@
 								</span>
 							</div>
 							<a href="">
-								<img src="images/blog/blog-two.jpg" alt="">
+								<img src="{{asset('images/blog/blog-two.jpg')}}" alt="">
 							</a>
 							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
-							<a  class="btn btn-primary" href="">Read More</a>
+							
+						    <a  class="btn btn-primary" href="{{url("/blog/post/1")}}">Read More</a>
+
 						</div>
 						<div class="single-blog-post">
 							<h3>Girls Pink T Shirt arrived in store</h3>
@@ -71,10 +73,10 @@
 								</span>
 							</div>
 							<a href="">
-								<img src="images/blog/blog-three.jpg" alt="">
+								<img src="{{asset('images/blog/blog-three.jpg')}}" alt="">
 							</a>
 							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
-							<a  class="btn btn-primary" href="">Read More</a>
+							<a  class="btn btn-primary" href="{{url("/blog/post/2")}}">Read More</a>
 						</div>
 						<div class="pagination-area">
 							<ul class="pagination">
